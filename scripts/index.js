58e1c90cca7ef7f60830ba24f5fa0198185255d9
Irving Tapia <129625494+JUANIRVING16@users.noreply.github.com>
2025-03-10 16:40:23 -0600

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+function gato_nav(){
+    window.location.href='pages/gato.html';
+}
+
+function jump_nav(){
+    window.location.href='pages/jump.html';
+}
+
+function piedra_nav(){
+    window.location.href='pages/piedra.html';
+}
+
+function flapy_nav(){
+    window.location.href='pages/flapy.html';
+}
